cd875555d41e19d1e1993306d209182a163c7ecf
Mark Ono <markeo.py@gmail.com>
2026-01-02 21:33:47 -0500

    Solution and tests for easy array problem - count elements greater than average of all previous elements.

---
+++ test/easy/arrays/count-elements-greater-than-prev-avg/solution1Test.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Random;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class solution1Test {
+    // Edge Cases
+    @Test
+    public void emptyArray() {
+        Assert.assertEquals(0, Result.countResponseTimeRegressions(java.util.Collections.emptyList()));
+    }
+
+    @Test
+    public void singleElementArray() {
+        ArrayList<Integer> numList = new ArrayList<>();
+        numList.add(1);
+
+        Assert.assertEquals(0, Result.countResponseTimeRegressions(numList));
+    }
+
+    @Test
+    public void strictlyDecreasingArray() {
+        ArrayList<Integer> decreasingArr = new ArrayList<>();
+        for (int i = 10; i > 0; i--) {
+            decreasingArr.add(i);
+        }
+
+        Assert.assertEquals(0, Result.countResponseTimeRegressions(decreasingArr));
+    }
+
+    @Test
+    public void strictlyIncreasingArray() {
+        ArrayList<Integer> increasingArr = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            increasingArr.add(i);
+        }
+
+        Assert.assertEquals(increasingArr.size() - 1, Result.countResponseTimeRegressions(increasingArr));
+    }
+    
+}
